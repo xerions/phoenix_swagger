e1f19c6d25c09f4192f5ab645e030238da9ba656
Yury Gargay <yury.gargay@gmail.com>
2016-08-19 13:00:22 +0300

    Version 0.1.1
    Some refactors for validator

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixSwagger.Mixfile do
 
   def project do
     [app: :phoenix_swagger,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
