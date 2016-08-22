61253471ba49b86c8dca7bee03dec5f0dfd84c85
Yury Gargay <yury.gargay@gmail.com>
2016-08-22 20:04:15 +0300

    Version 0.1.3 with correct posion version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixSwagger.Mixfile do
 
   def project do
     [app: :phoenix_swagger,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
