956b6d22cb96b43c41a2557a3b9b7539ebcb4e09
Alexander Kuleshov <kuleshovmail@gmail.com>
2016-12-09 16:46:41 +0600

    validator: add support for multiply schemas
    This patch provides additional clause for `parse_swagger_schema/1`
    function to read set of swagger schemas. After reading of list of
    swagger schemas, validator merges them into one.
    
    So other behaviour of validator is the same as before.

--- lib/phoenix_swagger/validator.ex
+++ lib/phoenix_swagger/validator.ex
@@ -26,8 +26,8 @@ defmodule PhoenixSwagger.Validator do
   @table :validator_table
 
   @doc """
-  The `parse_swagger_schema/1` takes path to a swagger schema, parses it
-  into ex_json_schema format and store to the `validator_table` ets
+  The `parse_swagger_schema/1` takes path or list of paths to a swagger schema(s), 
+  parses it/them into ex_json_schema format and store to the `validator_table` ets
   table.
 
   Usage:
@@ -46,21 +46,57 @@ defmodule PhoenixSwagger.Validator do
       }]
 
   """
-  def parse_swagger_schema(spec) do
-    schema = File.read(spec) |> elem(1) |> Poison.decode() |> elem(1)
-    # get rid from all keys besides 'paths' and 'definitions' as we
-    # need only in these fields for validation
-    schema = Enum.reduce(schema, %{}, fn(map, acc) ->
-      {key, val} = map
-      if key in ["paths", "definitions"] do
-        Map.put_new(acc, key, val)
-      else
-        acc
-      end      
+  def parse_swagger_schema(specs) when is_list(specs) do
+    schemas = Enum.map(specs, fn (spec) ->
+      read_swagger_schema(spec)
     end)
+    schema = Enum.reduce(schemas, %{}, fn(schema, acc) ->
+      acc = if acc["paths"] == nil do
+              Map.merge(acc, schema)
+            else
+              acc = Map.update!(acc, "paths", fn(paths_map) -> Map.merge(paths_map, schema["paths"]) end)
+              Map.update!(acc, "definitions", fn(definitions_map) -> Map.merge(definitions_map, schema["definitions"]) end)
+            end
+      acc
+    end)
+    collect_schema_attrs(schema)
+  end
+  def parse_swagger_schema(spec) do
+    schema = read_swagger_schema(spec)
+    collect_schema_attrs(schema)
+  end
 
-    # parse swagger schema
-    schema = Enum.map(schema["paths"], fn({path, data}) ->
+  @doc """
+  The `validate/2` takes a resource path and input parameters
+  of this resource.
+
+  Returns `:ok` in a case when parameters are valid for the
+  given resource or:
+
+    * {:error, :resource_not_exists} in a case when path is not
+      exists in the validator table;
+    * {:error, error_message, path} in a case when at least
+      one  parameter is not valid for the given resource.
+  """
+  def validate(path, params) do
+    case :ets.lookup(@table, path) do
+      [] ->
+        {:error, :resource_not_exists}
+      [{_, schema}] ->
+        case ExJsonSchema.Validator.validate(schema, params) do
+          :ok ->
+            :ok
+          {:error, [{error, path}]} ->
+            {:error, error, path}
+          {:error, error} ->
+            {:error, error, path}
+        end
+    end
+  end
+
+  @doc false
+  defp collect_schema_attrs(schema) do
+    Enum.map(schema["paths"], fn({path, data}) ->
       Enum.map(Map.keys(data), fn(method) ->
         parameters = data[method]["parameters"]
         # we may have a request without parameters, so nothing to validate
@@ -110,35 +146,6 @@ defmodule PhoenixSwagger.Validator do
         end
       end)
     end) |> List.flatten
-    schema
-  end
-
-  @doc """
-  The `validate/2` takes a resource path and input parameters
-  of this resource.
-
-  Returns `:ok` in a case when parameters are valid for the
-  given resource or:
-
-    * {:error, :resource_not_exists} in a case when path is not
-      exists in the validator table;
-    * {:error, error_message, path} in a case when at least
-      one  parameter is not valid for the given resource.
-  """
-  def validate(path, params) do
-    case :ets.lookup(@table, path) do
-      [] ->
-        {:error, :resource_not_exists}
-      [{_, schema}] ->
-        case ExJsonSchema.Validator.validate(schema, params) do
-          :ok ->
-            :ok
-          {:error, [{error, path}]} ->
-            {:error, error, path}
-          {:error, error} ->
-            {:error, error, path}
-        end
-    end
   end
 
   @doc false
@@ -197,6 +204,21 @@ defmodule PhoenixSwagger.Validator do
           ref_acc = Map.delete(ref_acc, parameter["name"])
           |> Map.put_new(parameter["name"], map)
         end
+      end
+    end)
+  end
+
+  @doc false
+  defp read_swagger_schema(file) do
+    schema = File.read(file) |> elem(1) |> Poison.decode() |> elem(1)
+    # get rid from all keys besides 'paths' and 'definitions' as we
+    # need only in these fields for validation
+    Enum.reduce(schema, %{}, fn(map, acc) ->
+      {key, val} = map
+      if key in ["paths", "definitions"] do
+        Map.put_new(acc, key, val)
+      else
+        acc
       end
     end)
   end
