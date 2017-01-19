be2d37ae56f6a01016459036c2d9b0158bdb103b
Yury Gargay <yury.gargay@gmail.com>
2017-01-19 12:42:51 +0100

    Version 0.3.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixSwagger.Mixfile do
 
   def project do
     [app: :phoenix_swagger,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
