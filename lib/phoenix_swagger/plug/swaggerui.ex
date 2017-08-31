05547591c1e38b2a7ed550cd5ffe492aa605326b
Stuart Terrett <shterrett@gmail.com>
2017-08-31 10:03:30 -0400

    Namespace v2 and v3 swagger-ui files

--- lib/phoenix_swagger/plug/swaggerui.ex
+++ lib/phoenix_swagger/plug/swaggerui.ex
@@ -34,33 +34,33 @@ defmodule PhoenixSwagger.Plug.SwaggerUI do
     <meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="IE=edge">
     <title>Swagger UI</title>
-    <link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />
-    <link href='css/typography.css' media='screen' rel='stylesheet' type='text/css'/>
-    <link href='css/reset.css' media='screen' rel='stylesheet' type='text/css'/>
-    <link href='css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
-    <link href='css/reset.css' media='print' rel='stylesheet' type='text/css'/>
-    <link href='css/print.css' media='print' rel='stylesheet' type='text/css'/>
+    <link rel="icon" type="image/png" href="v2/images/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="v2/images/favicon-16x16.png" sizes="16x16" />
+    <link href='v2/css/typography.css' media='screen' rel='stylesheet' type='text/css'/>
+    <link href='v2/css/reset.css' media='screen' rel='stylesheet' type='text/css'/>
+    <link href='v2/css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
+    <link href='v2/css/reset.css' media='print' rel='stylesheet' type='text/css'/>
+    <link href='v2/css/print.css' media='print' rel='stylesheet' type='text/css'/>
 
-    <script src='lib/object-assign-pollyfill.js' type='text/javascript'></script>
-    <script src='lib/jquery-1.8.0.min.js' type='text/javascript'></script>
-    <script src='lib/jquery.slideto.min.js' type='text/javascript'></script>
-    <script src='lib/jquery.wiggle.min.js' type='text/javascript'></script>
-    <script src='lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
-    <script src='lib/handlebars-4.0.5.js' type='text/javascript'></script>
-    <script src='lib/lodash.min.js' type='text/javascript'></script>
-    <script src='lib/backbone-min.js' type='text/javascript'></script>
-    <script src='swagger-ui.js' type='text/javascript'></script>
-    <script src='lib/highlight.9.1.0.pack.js' type='text/javascript'></script>
-    <script src='lib/highlight.9.1.0.pack_extended.js' type='text/javascript'></script>
-    <script src='lib/jsoneditor.min.js' type='text/javascript'></script>
-    <script src='lib/marked.js' type='text/javascript'></script>
-    <script src='lib/swagger-oauth.js' type='text/javascript'></script>
+    <script src='v2/lib/object-assign-pollyfill.js' type='text/javascript'></script>
+    <script src='v2/lib/jquery-1.8.0.min.js' type='text/javascript'></script>
+    <script src='v2/lib/jquery.slideto.min.js' type='text/javascript'></script>
+    <script src='v2/lib/jquery.wiggle.min.js' type='text/javascript'></script>
+    <script src='v2/lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
+    <script src='v2/lib/handlebars-4.0.5.js' type='text/javascript'></script>
+    <script src='v2/lib/lodash.min.js' type='text/javascript'></script>
+    <script src='v2/lib/backbone-min.js' type='text/javascript'></script>
+    <script src='v2/swagger-ui.js' type='text/javascript'></script>
+    <script src='v2/lib/highlight.9.1.0.pack.js' type='text/javascript'></script>
+    <script src='v2/lib/highlight.9.1.0.pack_extended.js' type='text/javascript'></script>
+    <script src='v2/lib/jsoneditor.min.js' type='text/javascript'></script>
+    <script src='v2/lib/marked.js' type='text/javascript'></script>
+    <script src='v2/lib/swagger-oauth.js' type='text/javascript'></script>
 
     <!-- Some basic translations -->
-    <!-- <script src='lang/translator.js' type='text/javascript'></script> -->
-    <!-- <script src='lang/ru.js' type='text/javascript'></script> -->
-    <!-- <script src='lang/en.js' type='text/javascript'></script> -->
+    <!-- <script src='v2/lang/translator.js' type='text/javascript'></script> -->
+    <!-- <script src='v2/lang/ru.js' type='text/javascript'></script> -->
+    <!-- <script src='v2/lang/en.js' type='text/javascript'></script> -->
 
     <script type="text/javascript">
       $(function () {
@@ -124,7 +124,7 @@ defmodule PhoenixSwagger.Plug.SwaggerUI do
   <body class="swagger-section">
   <div id='header'>
     <div class="swagger-ui-wrap">
-      <a id="logo" href="http://swagger.io"><img class="logo__img" alt="swagger" height="30" width="30" src="images/logo_small.png" /><span class="logo__title">swagger</span></a>
+      <a id="logo" href="http://swagger.io"><img class="logo__img" alt="swagger" height="30" width="30" src="v2/images/logo_small.png" /><span class="logo__title">swagger</span></a>
       <form id='api_selector'>
         <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl" type="text"/></div>
         <div id='auth_container'></div>
