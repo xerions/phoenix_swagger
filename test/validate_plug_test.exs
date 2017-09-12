59c3d89216fd1d5af7c996b2d12d3d29d8ffb90a
Leonid Batyuk <leonid.batyuk@meltwater.com>
2017-09-12 14:54:18 +0200

    Don't use 'describe' for backwards compat

--- test/validate_plug_test.exs
+++ test/validate_plug_test.exs
@@ -22,38 +22,36 @@ defmodule ValidatePlugTest do
     assert opts == Validate.init(opts)
   end
 
-  describe "call" do
-    test "validation successful on a valid request" do
-      test_conn = init_conn(:get, "/api/pets")
-      test_conn = Validate.call(test_conn, [])
-      assert is_nil test_conn.status
-      assert is_nil test_conn.resp_body
-      assert test_conn.private[:phoenix_swagger][:valid]
-    end
+  test "validation successful on a valid request" do
+    test_conn = init_conn(:get, "/api/pets")
+    test_conn = Validate.call(test_conn, [])
+    assert is_nil test_conn.status
+    assert is_nil test_conn.resp_body
+    assert test_conn.private[:phoenix_swagger][:valid]
+  end
 
-    test "validation fails on an invalid request" do
-      test_conn = init_conn(:post, "/v1/products", %{foo: :bar})
-      test_conn = Validate.call(test_conn, [])
-      assert {400, _, _} = sent_resp(test_conn)
-    end
+  test "validation fails on an invalid request" do
+    test_conn = init_conn(:post, "/v1/products", %{foo: :bar})
+    test_conn = Validate.call(test_conn, [])
+    assert {400, _, _} = sent_resp(test_conn)
+  end
 
-    test "validation fails on an invalid path" do
-      test_conn = init_conn(:get, "foo", %{foo: :bar})
-      test_conn = Validate.call(test_conn, [])
-      assert {404, _, _} = sent_resp(test_conn)
-    end
+  test "validation fails on an invalid path" do
+    test_conn = init_conn(:get, "foo", %{foo: :bar})
+    test_conn = Validate.call(test_conn, [])
+    assert {404, _, _} = sent_resp(test_conn)
+  end
 
-    test "validation fails with custom code" do
-      test_conn = init_conn(:post, "/v1/products", %{foo: :bar})
-      test_conn = Validate.call(test_conn, [validation_failed_status: 422])
-      assert {422, _, _} = sent_resp(test_conn)
-    end
+  test "validation fails with custom code" do
+    test_conn = init_conn(:post, "/v1/products", %{foo: :bar})
+    test_conn = Validate.call(test_conn, [validation_failed_status: 422])
+    assert {422, _, _} = sent_resp(test_conn)
+  end
 
-    test "validation skipped if valid flag is already set" do
-      test_conn = init_conn(:get, "foo", %{foo: :bar})
-      test_conn = Conn.put_private(test_conn, :phoenix_swagger, %{valid: true})
-      assert test_conn == Validate.call(test_conn, [])
-    end
+  test "validation skipped if valid flag is already set" do
+    test_conn = init_conn(:get, "foo", %{foo: :bar})
+    test_conn = Conn.put_private(test_conn, :phoenix_swagger, %{valid: true})
+    assert test_conn == Validate.call(test_conn, [])
   end
 
   defp init_conn(verb, path, body_params \\ %{}, path_params \\ %{}) do
