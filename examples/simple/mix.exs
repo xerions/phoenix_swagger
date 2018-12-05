af2513d27ec8e37e654a3c8b1b9002d6e049f0ed
Bartosz Górka <bartosz.gorka@interia.eu>
2018-12-05 12:47:06 +0100

    Change Elixir version to 1.6

--- examples/simple/mix.exs
+++ examples/simple/mix.exs
@@ -4,7 +4,7 @@ defmodule Simple.Mixfile do
   def project do
     [app: :simple,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.6",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers ++ [:phoenix_swagger],
      build_embedded: Mix.env == :prod,
