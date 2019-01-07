b0a4c8cbc7ff5ed0a632e9d3671a64a77e43e325
Tom Crossland <tom.crossland@bluetab.net>
2019-01-07 04:33:22 +0100

    chore: poison is an optional dependency

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule PhoenixSwagger.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:poison, "~> 2.2 or ~> 3.0"},
+      {:poison, "~> 2.2 or ~> 3.0", optional: true},
       {:jason, "~> 1.0", optional: true},
       {:ex_json_schema, "~> 0.5", optional: true},
       {:plug, "~> 1.4"},
