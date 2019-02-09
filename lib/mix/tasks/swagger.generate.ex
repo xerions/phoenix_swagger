c7c24dd65bbbba518dd79479361731e87b9dbc21
Samuel Lissner <samuel.lissner@treefin.com>
2019-02-09 19:08:53 +0100

    Derive schemes from endpoint :url

--- lib/mix/tasks/swagger.generate.ex
+++ lib/mix/tasks/swagger.generate.ex
@@ -180,12 +180,20 @@ defmodule Mix.Tasks.Phx.Swagger.Generate do
     url = Keyword.get(endpoint_config, :url)
     host = Keyword.get(url, :host, "localhost")
     port = Keyword.get(url, :port, 4000)
+    scheme = Keyword.get(url, :scheme, :http)
 
     swagger_map =
       if (!load_from_system_env) and is_binary(host) and (is_integer(port) or is_binary(port)) do
         Map.put_new(swagger_map, :host, "#{host}:#{port}")
       else
         swagger_map # host / port may be {:system, "ENV_VAR"} tuples or loaded in Endpoint.init callback
+      end
+
+    swagger_map =
+      if scheme == :https do
+        Map.put_new(swagger_map, :schemes, ["https", "http"])
+      else
+        swagger_map
       end
 
     case endpoint_config[:https] do
