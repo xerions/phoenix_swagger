773a709bf0f5c5a66f8e74db48ff78185d0aa1de
Jason Dew <jason.dew@gmail.com>
2019-05-08 15:37:17 -0400

    Add sorting to tags and methods

--- lib/phoenix_swagger/plug/swaggerui.ex
+++ lib/phoenix_swagger/plug/swaggerui.ex
@@ -22,10 +22,10 @@ defmodule PhoenixSwagger.Plug.SwaggerUI do
   alias Plug.Conn
 
   # Serve static assets before routing
-  plug Plug.Static, at: "/", from: :phoenix_swagger
+  plug(Plug.Static, at: "/", from: :phoenix_swagger)
 
-  plug :match
-  plug :dispatch
+  plug(:match)
+  plug(:dispatch)
 
   @template """
   <!-- HTML for static distribution bundle build -->
@@ -110,6 +110,19 @@ defmodule PhoenixSwagger.Plug.SwaggerUI do
       url: swagger_url.href,
       dom_id: '#swagger-ui',
       deepLinking: true,
+      tagsSorter: (a, b) => {
+        return a.localeCompare(b);
+      },
+      operationsSorter: (a, b) => {
+        var methodsOrder = ["get", "post", "put", "patch", "delete", "options"];
+        var result = methodsOrder.indexOf( a.get("method") ) - methodsOrder.indexOf( b.get("method") );
+
+        if (result === 0) {
+          result = a.get("path").localeCompare(b.get("path"));
+        }
+
+        return result;
+      },
       presets: [
         SwaggerUIBundle.presets.apis,
         SwaggerUIStandalonePreset
@@ -131,6 +144,7 @@ defmodule PhoenixSwagger.Plug.SwaggerUI do
   # Redirect / to /index.html
   get "/" do
     base_path = conn.request_path |> String.trim_trailing("/")
+
     conn
     |> Conn.put_resp_header("location", "#{base_path}/index.html")
     |> Conn.put_resp_content_type("text/html")
@@ -146,6 +160,7 @@ defmodule PhoenixSwagger.Plug.SwaggerUI do
   # Render the swagger.json file or 404 for any other file
   get "/*paths" do
     spec_url = conn.assigns.spec_url
+
     case conn.path_params["paths"] do
       [^spec_url] -> Conn.send_file(conn, 200, conn.assigns.swagger_file_path)
       _ -> Conn.send_resp(conn, 404, "not found")
